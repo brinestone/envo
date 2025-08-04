d8f640c3da6d28e0bb53f3a8f8811e3246d28e15
Conrad Bekondo <conradbekondo@gmail.com>
2025-08-04 02:33:51 +0100

    feat(sheet): add HlmSheet component and related directives for enhanced modal functionality

--- apps/console/src/app/pages/project/all-projects/all-projects.page.ts
+++ apps/console/src/app/pages/project/all-projects/all-projects.page.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucidePlus } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/helm/button';
+import { BrnSheetContentDirective, BrnSheetTriggerDirective } from '@spartan-ng/brain/sheet';
+import {
+  HlmSheetModule
+} from '@spartan-ng/helm/sheet';
 
 @Component({
+  viewProviders: [
+    provideIcons({
+      lucidePlus
+    })
+  ],
   selector: 'ev-all-projects',
-  imports: [],
+  imports: [
+    BrnSheetContentDirective,
+    BrnSheetTriggerDirective,
+    HlmButtonDirective,
+    HlmSheetModule,
+    NgIcon
+  ],
   templateUrl: './all-projects.page.html',
   styleUrl: './all-projects.page.scss'
 })
 export class AllProjectsPage {
+  onCreateProjectButtonClicked() {
 
+  }
 }
