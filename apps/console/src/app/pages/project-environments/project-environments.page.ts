e6639bdbd570aa5492969eb6157ab2d1a7148b99
Conrad Bekondo <conradbekondo@gmail.com>
2025-08-25 16:10:17 +0100

    feat: update routing for configurations and add project environments page

---
+++ apps/console/src/app/pages/project-environments/project-environments.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ev-project-environments',
+  imports: [],
+  templateUrl: './project-environments.page.html',
+  styleUrl: './project-environments.page.scss'
+})
+export class ProjectEnvironmentsPage {
+
+}
